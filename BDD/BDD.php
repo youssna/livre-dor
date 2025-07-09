68c871f3deeab11f2318bee1877668718bf9a7ba
youssna <nasefyoussef@outlook.com>
2025-07-09 11:53:32 +0200

    Update BDD.php

--- BDD/BDD.php
+++ BDD/BDD.php
@@ -1,8 +1,12 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 try {
-    $bdd = new PDO('mysql:host=localhost;dbname=livres_or', 'root', 'root');
+    $bdd = new PDO('mysql:host=localhost;dbname=livre_or', 'user_ecole', 'mdp_ecole');
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
+    die("❌ Erreur de connexion à la base : " . $e->getMessage());
 }
 ?>
